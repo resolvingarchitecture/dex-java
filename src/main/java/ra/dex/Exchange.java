aac12bbad29224db7afb7a555e29a042ee7b6b2b
objectorange <brian@resolvingarchitecture.io>
2021-08-16 10:13:50 -0400

    added exchange to track offer, escrow, maker, taker

---
+++ src/main/java/ra/dex/Exchange.java
@@ -0,0 +1,12 @@
+package ra.dex;
+
+import ra.common.identity.DID;
+
+public class Exchange {
+
+    public Offer offer;
+    public Escrow escrow;
+    public DID maker;
+    public DID taker;
+
+}
