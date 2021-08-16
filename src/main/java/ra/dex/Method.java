bdf1042d421d6180b1f30deb3ce6ddc9d3d9c2f8
objectorange <brian@resolvingarchitecture.io>
2021-08-16 10:14:59 -0400

    add a method enum for methods of exchange

---
+++ src/main/java/ra/dex/Method.java
@@ -0,0 +1,11 @@
+package ra.dex;
+
+public enum Method {
+    F2F,
+    InteracETransfer,
+    SEPA_FI, // Finland
+    SEPA_PT, // Portugal
+    SEPA_SE, // Sweden
+    SEPA_SK, // Slovakia
+    Zelle
+}
