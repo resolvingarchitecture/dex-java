cf504721b4ca35455a88cb8d09da0d73ef60397f
objectorange <brian@resolvingarchitecture.io>
2021-08-16 10:14:17 -0400

    added request and response for exchange

---
+++ src/main/java/ra/dex/ExchangeRequest.java
@@ -0,0 +1,28 @@
+package ra.dex;
+
+import ra.common.JSONSerializable;
+
+import java.util.Map;
+
+public class ExchangeRequest implements JSONSerializable {
+
+    @Override
+    public Map<String, Object> toMap() {
+        return null;
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> map) {
+
+    }
+
+    @Override
+    public String toJSON() {
+        return null;
+    }
+
+    @Override
+    public void fromJSON(String s) {
+
+    }
+}
