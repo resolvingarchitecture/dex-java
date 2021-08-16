af0b17d1d8173267aa11aa8a5f4a31ced6a65c5d
objectorange <brian@resolvingarchitecture.io>
2021-08-16 10:13:21 -0400

    will need escrow

---
+++ src/main/java/ra/dex/Escrow.java
@@ -0,0 +1,28 @@
+package ra.dex;
+
+import ra.common.JSONSerializable;
+import ra.util.JSONParser;
+import ra.util.JSONPretty;
+
+import java.util.Map;
+
+public class Escrow implements JSONSerializable {
+
+    @Override
+    public Map<String, Object> toMap() {
+        return null;
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> map) {
+
+    }
+
+    public String toJSON() {
+        return JSONPretty.toPretty(JSONParser.toString(this.toMap()), 4);
+    }
+
+    public void fromJSON(String json) {
+        this.fromMap((Map)JSONParser.parse(json));
+    }
+}
