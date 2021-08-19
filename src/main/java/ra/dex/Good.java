713c275c2ba9c4624090e514e0447a96057ae9a0
objectorange <brian@resolvingarchitecture.io>
2021-08-19 10:25:48 -0400

    implemented

--- src/main/java/ra/dex/Good.java
+++ src/main/java/ra/dex/Good.java
@@ -4,18 +4,47 @@ import ra.common.JSONSerializable;
 import ra.util.JSONParser;
 import ra.util.JSONPretty;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Good implements JSONSerializable {
 
-    @Override
-    public Map<String, Object> toMap() {
-        return null;
+    private Map<String,Object> toExchange;
+
+    public Good() {
+        this.toExchange = new HashMap<>();
+        toExchange.put("id", UUID.randomUUID());
+    }
+
+    public Good(Map<String,Object> toExchange) {
+        if(toExchange.get("id")==null) {
+            toExchange.put("id", UUID.randomUUID());
+        }
+        this.toExchange = toExchange;
+    }
+
+    public void setToExchange(Map<String,Object> toExchange) {
+        if(toExchange.get("id")==null) {
+            toExchange.put("id", UUID.randomUUID());
+        }
+        this.toExchange = toExchange;
+    }
+
+    public Object getToExchange() {
+        return toExchange;
     }
 
     @Override
-    public void fromMap(Map<String, Object> map) {
+    public Map<String, Object> toMap() {
+        Map<String,Object> m = new HashMap<>();
+        if(toExchange!=null) m.put("toExchange", toExchange);
+        return m;
+    }
 
+    @Override
+    public void fromMap(Map<String, Object> m) {
+        if(m.get("toExchange")!=null) toExchange = (Map<String, Object>) m.get("toExchange");
     }
 
     public String toJSON() {
@@ -24,5 +53,10 @@ public class Good implements JSONSerializable {
 
     public void fromJSON(String json) {
         this.fromMap((Map)JSONParser.parse(json));
+    }
+
+    @Override
+    public String toString() {
+        return toJSON();
     }
 }
