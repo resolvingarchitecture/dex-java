09aa693bfb31ddbc78c41b64d996096eaaa12d11
objectorange <brian@resolvingarchitecture.io>
2021-08-20 14:23:59 -0400

    added default lists and method

--- src/main/java/ra/dex/Offer.java
+++ src/main/java/ra/dex/Offer.java
@@ -4,17 +4,59 @@ import ra.common.JSONSerializable;
 import ra.util.JSONParser;
 import ra.util.JSONPretty;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Offer implements JSONSerializable {
 
-    public List<Good> from;
-    public List<Good> to;
+    private List<Good> from = new ArrayList<>();
+    private List<Good> to = new ArrayList<>();
+    private Method method;
+
+    public Offer() {
+
+    }
+
+    public Offer(Good from, Good to, Method method) {
+        this.from.add(from);
+        this.to.add(to);
+        this.method = method;
+    }
+
+    public Offer(List<Good> from, List<Good> to, Method method) {
+        this.from = from;
+        this.to = to;
+        this.method = method;
+    }
+
+    public List<Good> getFrom() {
+        return from;
+    }
+
+    public void setFrom(List<Good> from) {
+        this.from = from;
+    }
+
+    public List<Good> getTo() {
+        return to;
+    }
+
+    public void setTo(List<Good> to) {
+        this.to = to;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
 
     @Override
     public Map<String, Object> toMap() {
-        return null;
+        Map<String,Object> m = new HashMap<>();
+
+        return m;
     }
 
     @Override
